ff4d03dbc4557f7a3995111c4a2f76db7d011c66
aidenbohlander <aiden.bohlander@gmail.com>
2024-07-09 21:27:04 -0700

    Switch to flexbox layout, add some inputs

--- server.py
+++ server.py
@@ -1,11 +1,33 @@
 from flask import Flask, render_template
 
 app = Flask(__name__)
+session = False
+sessionstart = None
 
 
 @app.route("/")
 def main():
     return render_template("index.html")
+
+
+@app.route("/startsession")
+def start():
+    pass
+
+
+@app.route("/endsession")
+def end():
+    pass
+
+
+@app.route("/time")
+def get_time():
+    pass
+
+
+@app.route("/checksession")
+def get_session():
+    pass
 
 
 if __name__ == "__main__":
